7347bc7a33673ed13c34045a105966cfaa45474c
MarcusVinicius14 <marcus.vas14@gmail.com>
2023-03-30 16:19:27 -0300

    commit 4

--- src/services/photos.js
+++ src/services/photos.js
@@ -1,9 +1,11 @@
+
 export const getPhotos = async (photosPerPage,callBack) =>{
 
     const apiKey = import.meta.env.VITE_KEY_API_PEXELS;
     const baseUrl = 'https://api.pexels.com/v1/curated'
 
     let response =[]
+
     try {
         response = await fetch(`${baseUrl}?page=1&per_page=${photosPerPage}`,
         {
